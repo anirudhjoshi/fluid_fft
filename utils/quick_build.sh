642d1e83418d21c6e8eb788a8223f6ffed7a6542
Anirudh Joshi <github@anirudhjoshi.com>
2012-09-17 18:04:44 +1000

    added fav code

--- utils/quick_build.sh
+++ utils/quick_build.sh
@@ -6,3 +6,4 @@ cat ../src/js/fluid.js ../src/js/colors.js ../src/js/main.js > ../build/f.js
 cat ../src/css/main.css > ../build/f.css
 
 cp -f ../src/index.html ../build/index.html
+cp -f ../src/favicon.ico ../build/favicon.ico
